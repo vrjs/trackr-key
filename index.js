ae88d6700a95e902d8cdc48339079a9998c6e0ae
Scott Frees <scott.frees@gmail.com>
2015-07-15 13:32:05 -0400

    version bump

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.make = function(THREE) {
-    var channels = [];
+    var sensors = [];
     for (var i = 0; i < 10; i++ ) {
-        channels.push(
+        sensors.push(
         {
             controlled: [],
             position: new THREE.Vector3(0, 0, 0),
@@ -12,38 +12,38 @@ exports.make = function(THREE) {
     }
     var tracker = {
         active : 0, 
-        channels : channels,
+        sensors : sensors,
         poll: function() {
             this.apply_poll();
         },
-        add: function(obj, channel) {
-            channel = channel || 0
-            this.channels[channel].controlled.push(obj);
+        add: function(obj, sensor) {
+            sensor = sensor || 0
+            this.sensors[sensor].controlled.push(obj);
         },
-        remove: function(obj, channel) {
-            channel = channel || 0
-            this.channels[channel].controlled.remove(obj);
+        remove: function(obj, sensor) {
+            sensor = sensor || 0
+            this.sensors[sensor].controlled.remove(obj);
         },
-        scale : function (channel, scale) {
+        scale : function (sensor, scale) {
             if (scale === undefined ) {
-                return this.channels[channel].scale;
+                return this.sensors[sensor].scale;
             }
             else {
-                this.channels[channel].scale = scale;
+                this.sensors[sensor].scale = scale;
             }
         }, 
-        offset : function (channel, offset) {
+        offset : function (sensor, offset) {
             if (offset === undefined ) {
-                return this.channels[channel].offset;
+                return this.sensors[sensor].offset;
             }
             else {
-                this.channels[channel].offset = offset;
+                this.sensors[sensor].offset = offset;
             }
         },
         handle_key: function(event) {
             if (event.which >= 48 && event.which <= 57) {
                 this.active = event.which - 48;
-                console.log("Switched keyboard trackr channel to " + this.active);
+                console.log("Switched keyboard trackr sensor to " + this.active);
                 return;
             }
 
@@ -52,44 +52,44 @@ exports.make = function(THREE) {
                 quaternion.setFromAxisAngle(vector, sign * Math.PI / 2 / 20);
                 return quaternion
             }
-            var channel = this.channels[this.active];
+            var sensor = this.sensors[this.active];
             var ramount = Math.PI / 2 / 20;
 
             if (event.which === 88 && event.shiftKey) { // X moves + in x direction
-                channel.position.x += 1;
+                sensor.position.x += 1;
             } else if (event.which === 88) {
-                channel.position.x -= 1;
+                sensor.position.x -= 1;
             } else if (event.which === 89 && event.shiftKey) {
-                channel.position.y += 1;
+                sensor.position.y += 1;
             } else if (event.which === 89) {
-                channel.position.y -= 1;
+                sensor.position.y -= 1;
             } else if (event.which === 90 && event.shiftKey) {
-                channel.position.z += 1;
+                sensor.position.z += 1;
             } else if (event.which === 90) {
-                channel.position.z -= 1;
+                sensor.position.z -= 1;
             } else if (event.which == 72 && event.shiftKey) {
-                channel.orientation.multiply(make_rotation(new THREE.Vector3(0, 1, 0), 1));
+                sensor.orientation.multiply(make_rotation(new THREE.Vector3(0, 1, 0), 1));
             } else if (event.which == 72) {
-                channel.orientation.multiply(make_rotation(new THREE.Vector3(0, 1, 0), -1));
+                sensor.orientation.multiply(make_rotation(new THREE.Vector3(0, 1, 0), -1));
             } else if (event.which == 74 && event.shiftKey) {
-                channel.orientation.multiply(make_rotation(new THREE.Vector3(1, 0, 0), 1));
+                sensor.orientation.multiply(make_rotation(new THREE.Vector3(1, 0, 0), 1));
             } else if (event.which == 74) {
-                channel.orientation.multiply(make_rotation(new THREE.Vector3(1, 0, 0), -1));
+                sensor.orientation.multiply(make_rotation(new THREE.Vector3(1, 0, 0), -1));
             } else if (event.which == 78 && event.shiftKey) {
-                channel.orientation.multiply(make_rotation(new THREE.Vector3(0, 0, 1), 1));
+                sensor.orientation.multiply(make_rotation(new THREE.Vector3(0, 0, 1), 1));
             } else if (event.which == 78) {
-                channel.orientation.multiply(make_rotation(new THREE.Vector3(0, 0, 1), -1));
+                sensor.orientation.multiply(make_rotation(new THREE.Vector3(0, 0, 1), -1));
             }
         },
         apply_poll: function() {
             for (var c = 0; c < 10; c++ ) {
-                channel = this.channels[c];
-                for (var i = 0; i < channel.controlled.length; i++) {
-                    o = channel.controlled[i];
-                    o.position.x = channel.position.x * channel.scale.x + channel.offset.x;
-                    o.position.y = channel.position.y * channel.scale.y + channel.offset.y;
-                    o.position.z = channel.position.z * channel.scale.z + channel.offset.z;
-                    o.quaternion.copy(channel.orientation);
+                sensor = this.sensors[c];
+                for (var i = 0; i < sensor.controlled.length; i++) {
+                    o = sensor.controlled[i];
+                    o.position.x = sensor.position.x * sensor.scale.x + sensor.offset.x;
+                    o.position.y = sensor.position.y * sensor.scale.y + sensor.offset.y;
+                    o.position.z = sensor.position.z * sensor.scale.z + sensor.offset.z;
+                    o.quaternion.copy(sensor.orientation);
                 }
             }
         }
